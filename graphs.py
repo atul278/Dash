a7f36c0fb3ace581b8e614ce4d04c8c1da69ab8c
Atul Dewangan <atuldewangan03@gmail.com>
2018-05-22 11:16:13 +0530

    Add files via upload

---
+++ graphs.py
@@ -0,0 +1,61 @@
+import dash
+import dash_core_components as dcc
+import plotly.graph_objs as go
+import dash_html_components as html
+import numpy as np
+
+
+x = np.linspace(0, 20, 100)
+y = np.random.randn(100)
+app = dash.Dash()
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+dcc.Graph(
+    figure=go.Figure(
+        data=[
+	     go.Scatter(
+    x = x,
+    y = y,
+    mode = 'lines',
+    name = 'lines'
+)
+        ],
+        layout=go.Layout(
+            title='Random line plot',
+            showlegend=True,
+            legend=go.Legend(
+                x=0,
+                y=1.0
+            ),
+#            margin=go.Margin(l=40, r=0, t=40, b=30)
+        )
+    ),
+    style={'height': 300},
+    id='my-graph1'
+),
+
+dcc.Graph(
+    figure=go.Figure(
+        data=[
+            go.Pie(
+                labels=["friends","GOT","HIMYM","Breking bad","House of cards","Suits"],
+                values=[95, 98, 80, 97, 95, 93],
+                name='Rest of world',
+                )
+        ],
+        layout=go.Layout(
+            title='Tv series',
+            showlegend=True,
+            legend=go.Legend(
+                x=0,
+                y=1.0
+            ),
+            margin=go.Margin(l=40, r=0, t=40, b=30)
+        )
+    ),
+   style={'height': 300},
+    id='my-graph2'
+)
+])
+app.run_server(debug=True)
+
